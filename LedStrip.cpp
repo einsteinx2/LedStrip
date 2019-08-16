143d8a2b5ba1eb7956c6e3a981fd21d773bd5ddf
Ben Baron <ben@einsteinx2.com>
2019-08-16 10:05:01 -0500

    Dynamically allow led pins depending on board

--- LedStrip.cpp
+++ LedStrip.cpp
@@ -10,17 +10,69 @@ LedStrip::LedStrip(int numLeds, int dataPin, int defaultBrightness, uint8_t star
 
     // Get around C++ template limits to allow dynamically setting the data pin
     switch (dataPin) {
+        #if MAX_PIN > 0
         case  0: FastLED.addLeds<NEOPIXEL,  0>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 1
         case  1: FastLED.addLeds<NEOPIXEL,  1>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 2
         case  2: FastLED.addLeds<NEOPIXEL,  2>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 3
         case  3: FastLED.addLeds<NEOPIXEL,  3>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 4
         case  4: FastLED.addLeds<NEOPIXEL,  4>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 5
         case  5: FastLED.addLeds<NEOPIXEL,  5>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 6
         case  6: FastLED.addLeds<NEOPIXEL,  6>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 7
         case  7: FastLED.addLeds<NEOPIXEL,  7>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 8
         case  8: FastLED.addLeds<NEOPIXEL,  8>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 9
         case  9: FastLED.addLeds<NEOPIXEL,  9>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 10
         case 10: FastLED.addLeds<NEOPIXEL, 10>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 11
+        case 11: FastLED.addLeds<NEOPIXEL, 11>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 12
+        case 12: FastLED.addLeds<NEOPIXEL, 12>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 13
+        case 13: FastLED.addLeds<NEOPIXEL, 13>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 14
+        case 13: FastLED.addLeds<NEOPIXEL, 14>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 15
+        case 13: FastLED.addLeds<NEOPIXEL, 15>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 16
+        case 13: FastLED.addLeds<NEOPIXEL, 16>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 17
+        case 13: FastLED.addLeds<NEOPIXEL, 17>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 18
+        case 13: FastLED.addLeds<NEOPIXEL, 18>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 19
+        case 13: FastLED.addLeds<NEOPIXEL, 19>(leds, numLeds); break;
+        #endif
+        #if MAX_PIN > 20
+        case 13: FastLED.addLeds<NEOPIXEL, 20>(leds, numLeds); break;
+        #endif
     }
     
     FastLED.setBrightness(defaultBrightness);
